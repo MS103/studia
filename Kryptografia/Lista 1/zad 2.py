3c250d82131e5659d22090f23c8097fa93df0953
MS103 <48585230+MS103@users.noreply.github.com>
2019-03-15 10:20:39 +0100

    Update zad 2.py

--- Kryptografia/Lista 1/zad 2.py
+++ Kryptografia/Lista 1/zad 2.py
@@ -8,12 +8,12 @@ class GLIBC:
         for i in range(31, 34):
             self.states.append(self.states[i - 31])
         for i in range(34, 345):
-            self.states.append((self.states[i - 31] + self.states[i - 3]) % 2 ** 32)
+            self.states.append((self.states[i - 31] + self.states[i - 3]) % (2 ** 32))
 
     def random(self):
-        o = int(str(bin(self.states[-1])[:-1]), 2)
-        self.states.append((self.states[-31] + self.states[- 3]) % 2 ** 32)
-        return o
+        new_rand = (self.states[-3] + self.states[-31]) % (2 ** 32)
+        self.states.append(new_rand)
+        return new_rand >> 1
 
 
 ##CRACKER
@@ -21,7 +21,7 @@ def predict_new_value(numbers):
     if len(numbers) < 31:
         print('ERROR: za mało liczb!')
         exit(-1)
-    return ((numbers[-31] + numbers[-3]) % 2 ** 32, (numbers[-31] + numbers[-3] + 1) % 2 ** 32)
+    return (numbers[-31] + numbers[-3]) % 2 ** 31
 
 
 seed = 1
@@ -31,18 +31,19 @@ numbers = []
 for i in range(10000):
     numbers.append(glibc.random())
 if_predicted = []
+
 for k in range(10000):
-    predicted_numbers = predict_new_value(numbers)
+    predicted_number = predict_new_value(numbers)
     new_number = glibc.random()
     numbers.append(new_number)
-    if_predicted.append(new_number in predicted_numbers)
+    if_predicted.append(new_number == predicted_number)
 prob_of_win = sum(if_predicted)/len(if_predicted)
 print('Prawdopodobieństwo poprawnego zgadnięcia kolejnej liczby wynosi: ', prob_of_win)
-print('Czy prawdopodobieństwo jest znacząco większe od 0.5: ', prob_of_win > (1/2 + 1/2**10))
+
 # for j in range(10):
-#     predicted_numbers = predict_new_value(numbers)
-#     print('Podejrzewamy, że zostanie wylosowana jedna z tych dwóch liczb ', predicted_numbers)
+#     predicted_number = predict_new_value(numbers)
+#     print('Podejrzewamy, że zostanie wylosowana liczba: ', predicted_number)
 #     new_number = glibc.random()
 #     numbers.append(new_number)
 #     print('GLIBC wylosował: ', new_number)
-#     print('Czy cracker zgadł liczbę? ', new_number in predicted_numbers)
+#     print('Czy cracker zgadł liczbę? ', new_number == predicted_number)
